79968b1549f4c5d6a7c7aade27ecfa0ec312213d
Vaibhav Varshney <56710232+vaibhav-04@users.noreply.github.com>
2021-02-20 11:38:05 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
 	$servername = "localhost";
-        $username = "id16203220_root";
-        $password = "Svo{|W#>?hTa[ls5";
-        $dbname = "id16203220_hvsbank";
+        $username = "root";
+        $password = "";
+        $dbname = "hvsbank";
 
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
 
